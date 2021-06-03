09801a315416a0259ea77905317f4078803b6dd5
Hash466 <h466h@mail.ru>
2021-06-03 23:39:38 +0300

    Start dev

--- homework.py
+++ homework.py
@@ -14,17 +14,53 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    """
+    разобрать ответ API
+
+    Args:
+        homework (dict): данные с описанием, статусом и т.п. домашки
+
+    Returns:
+        (str): сообщение с текущим статусом домашки
+    """
+    homework_name = homework.get('homework_name')
+    status = homework.get('status')
+    if status == 'rejected':
+        return (f'У вас проверили работу "{homework_name}"!\n\n'
+                'К сожалению в работе нашлись ошибки.')
+    if status == 'approved':
+        return (f'У вас проверили работу "{homework_name}"!\n\n'
+                'Ревьюеру всё понравилось, можно приступать к следующему '
+                'уроку.')
+    if status == 'reviewing':
+        return (f'Ваша работа "{homework_name}" в данный момент проходит '
+                'ревью.\n\nМолитесь всем Богам, или предложите ревьюверу '
+                'бутылочку нормального пойла, дабы задобрить уважаемого!')
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    """
+    Запросить статус домашки через API
+
+    Args:
+        current_timestamp (int): начало интервала времени
+
+    Returns:
+        (dict): в зависимости от ответа возможно:
+            {'homeworks': [], 'current_date': 1622751338};
+            {'homeworks': [{...},], 'current_date': 1622752114}
+    """
+    headers = {
+        'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'
+    }
+    params = {
+        'from_date': current_timestamp
+    }
+    homework_statuses = requests.get(
+        'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
+        headers=headers,
+        params=params
+    )
     return homework_statuses.json()
 
 
@@ -35,13 +71,16 @@ def send_message(message, bot_client):
 
 def main():
     # проинициализировать бота здесь
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    # current_timestamp = int(time.time())  # начальное значение timestamp
+    # current_timestamp = (int(time.time()) - (5 * 60))
+    current_timestamp = 0
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
+            homeworks = new_homework.get('homeworks')
+            if homeworks:
+                send_message(parse_homework_status(homeworks[0]))
             current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
             time.sleep(300)  # опрашивать раз в пять минут
 
