3ce7f139e1cc6f3b1dcac422a4ab1a8ed570b5d5
Hash466 <g.h466h@gmail.com>
2021-06-08 20:12:38 +0300

    Save_4

--- homework.py
+++ homework.py
@@ -1,21 +1,23 @@
-from dotenv import load_dotenv
 import logging
-from logging.handlers import RotatingFileHandler
 import os
-import requests
 import time
+from logging.handlers import RotatingFileHandler
+
+import requests
+from json import JSONDecodeError
+from dotenv import load_dotenv
 from telegram import Bot
 
-
-load_dotenv()
-
-API_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
-LOG_FILENAME = 't_bot.log'
-LOG_PATH_NAME = os.sep.join([os.getcwd(), LOG_FILENAME])
+ERROR_DESCRIPTION = {
+    'ConnectionError': 'Ошибка соединения!\n{err}',
+    'TimeoutError': 'Время ожидание ответа истекло!\n{err}',
+    'JSONDecodeError': 'Ошибка разбора json ответа сервера!\n{err}',
+    'ResponseExcept': 'Ответ API содержит ошибку!\n{code}\n{message}',
+    'evn_error': 'Ошибка загрузки данных из ".env": {err}',
+    'status_error': 'Недокументированный статус работы "{homework_name}"!',
+    'name_error': 'Отсутствует имя домашки!',
+    'other_errors': 'При подключении к API возникла ошибка:\n{err}'
+}
 
 VERDICTS = {
     'rejected': ('У вас проверили работу "{homework_name}"!\n'
@@ -26,14 +28,6 @@ VERDICTS = {
     'reviewing': ('Ваша работа "{homework_name}" в данный момент проходит '
                   'ревью.\nМолитесь всем Богам, или предложите ревьюверу '
                   'бутылочку нормального пойла, дабы задобрить уважаемого!'),
-    'status_error': 'Недокументированный статус работы "{homework_name}"!'
-}
-
-ERROR_DESCRIPTION = {
-    'ConnectionError': 'Ошибка соединения!\n{err}',
-    'TimeoutError': 'Время ожидание ответа истекло!\n{err}',
-    'ExceptionJson': 'Ошибка разбора json ответа сервера!\n{err}',
-    'ResponseExcept': 'Ответ API содержит ошибку!\n{code}\n{message}',
 }
 
 LOG_MESSAGES = {
@@ -41,11 +35,27 @@ LOG_MESSAGES = {
     'bot_start': 'Бот запущен!',
     'bot_error': 'Бот столкнулся с ошибкой!:\n{err}',
 }
+LOG_FILENAME = 't_bot.log'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_PATH_NAME = os.path.join(BASE_DIR, LOG_FILENAME)
+
+load_dotenv()
+
+API_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+
+try:
+    PRAKTIKUM_TOKEN = os.environ['PRAKTIKUM_TOKEN']
+    TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+    CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
+except KeyError as err:
+    raise KeyError(ERROR_DESCRIPTION['evn_error'].format(err=err))
+
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 
 
 def parse_homework_status(homework):
     """
-    разобрать ответ API
+    Разобрать ответ API.
 
     Args:
         homework (dict): данные с описанием, статусом и т.п. домашки:
@@ -53,18 +63,23 @@ def parse_homework_status(homework):
     Returns:
         (str): сообщение с текущим статусом домашки
     """
-    homework_name = homework.get('homework_name', 'имя домашки не задано')
+    homework_name = homework.get('homework_name', 'no name')
+    if homework_name == 'no name':
+        raise ValueError(ERROR_DESCRIPTION['name_error'])
+
     status = homework.get('status', 'status_error')
-    if status == 'status_error':
+    if (status == 'status_error') or (status not in VERDICTS):
         raise ValueError(
-            VERDICTS[status].format(homework_name=homework_name)
+            ERROR_DESCRIPTION['status_error'].format(
+                homework_name=homework_name
+            )
         )
     return VERDICTS[status].format(homework_name=homework_name)
 
 
 def get_homework_statuses(current_timestamp):
     """
-    Запросить статус домашки через API
+    Запросить статус домашки через API.
 
     Args:
         current_timestamp (int): начало интервала времени
@@ -74,9 +89,7 @@ def get_homework_statuses(current_timestamp):
             {'homeworks': [], 'current_date': 1622751338};
             {'homeworks': [{...},], 'current_date': 1622752114}
     """
-    headers = {
-        'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'
-    }
+    headers = HEADERS
     params = {
         'from_date': current_timestamp
     }
@@ -95,12 +108,16 @@ def get_homework_statuses(current_timestamp):
         raise TimeoutError(
             ERROR_DESCRIPTION['TimeoutError'].format(err)
         )
+    except Exception as err:
+        raise Exception(
+            ERROR_DESCRIPTION['other_errors'].format(err)
+        )
 
     try:
         resp_json = homework_statuses.json()
-    except Exception as err:
-        raise Exception(
-            ERROR_DESCRIPTION['ExceptionJson'].format(err)
+    except JSONDecodeError as err:
+        raise JSONDecodeError(
+            ERROR_DESCRIPTION['JSONDecodeError'].format(err)
         )
 
     if 'homeworks' in resp_json:
@@ -120,12 +137,12 @@ def send_message(message, bot_client):
 
 def main():
     logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
 
     file_handler = RotatingFileHandler(
         LOG_PATH_NAME, maxBytes=50000000, backupCount=3
     )
-    file_handler.setLevel(logging.DEBUG)
+    file_handler.setLevel(logging.INFO)
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
@@ -135,7 +152,8 @@ def main():
 
     t_bot = Bot(token=TELEGRAM_TOKEN)
     logger.debug(LOG_MESSAGES['bot_start'])
-    current_timestamp = (int(time.time()) - (5 * 60))
+    current_timestamp = int(time.time())
+    #current_timestamp = 0
 
     while True:
         try:
@@ -147,8 +165,10 @@ def main():
                 logger.info(
                     LOG_MESSAGES['send_message'].format(message=message_text)
                 )
-            current_timestamp = new_homework.get('current_date',
-                                                 current_timestamp)
+            current_timestamp = (
+                new_homework.get('current_date', current_timestamp)
+                or current_timestamp
+            )
             time.sleep(300)
 
         except Exception as err:
@@ -157,12 +177,8 @@ def main():
 
 
 if __name__ == '__main__':
-
-    log_path = os.getcwd() + os.sep
-
     logging.basicConfig(
         level=logging.ERROR,
-        filename=LOG_PATH_NAME,
         filemode='w',
         format=(
             '%(asctime)s, %(levelname)s, %(name)s, %(filename)s, '
